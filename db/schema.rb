f214f4a6ea4d7d851369d7f85170b336665e901b
ryo_odo <marukae@gmail.com>
2021-12-05 14:08:12 +0000

    アドミン追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_125531) do
+ActiveRecord::Schema.define(version: 2021_12_05_140446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_125531) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["user_account_name"], name: "index_users_on_user_account_name", unique: true
   end
