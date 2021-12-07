7bf07f804e5b7049a125e457828a516ce2060abc
ryo_odo <marukae@gmail.com>
2021-12-07 08:37:46 +0000

    タグ機能、タグ検索機能実装

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,18 +4,9 @@ class GoalsController < ApplicationController
 
 
   def index
-
     @search = Goal.ransack(params[:q])
     @search.sorts = 'created_at desc'
     @goals = @search.result(distinct: true)
-    @condition = ""
-
-    if params[:q].present?
-      @condition = params[:q]
-    end
-
-
-
 
   end
 
@@ -24,8 +15,18 @@ class GoalsController < ApplicationController
   end
 
   def create
-    @goal = current_user.goals.build(goals_params)
+    @goal = current_user.goals.new(goals_params)
+    goal_tag_list = params[:goal][:goal_tag_name].gsub(/　/," ").strip.split(nil)
+
     if @goal.save
+      goal_tag_list.each do |goal_tag_name|
+        if GoalTag.exists?(goal_tag_name: goal_tag_name)
+          goal_tag_id = GoalTag.where(goal_tag_name: goal_tag_name).ids[0]
+          @goal.goal_taggings.create(goal_tag_id: goal_tag_id) #もしすでに同じタグがあったら、そのIDから中間テーブルだけ作る
+        else
+          @goal.goal_tags.create(goal_tag_name: goal_tag_name) #まだなかったらタグと中間テーブルのレコードを作る
+        end
+      end
       redirect_to goals_path, notice: "登録しました！"
     else
       render :new, notice: "登録に失敗しました"
@@ -49,5 +50,7 @@ class GoalsController < ApplicationController
   def goals_params
     params.require(:goal).permit(:goal_title, :description)
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root 'goals#index' #追記
-  resources :goals
-
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  
+  root 'goals#index'
+  resources :goals
+
+
 end
