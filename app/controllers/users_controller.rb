61418374875265698f2292aa16bd3bf3ae86e742
ryo_odo <marukae@gmail.com>
2021-12-08 09:11:40 +0000

    ユーザー検索機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @search = User.ransack(params[:q])
+    @search.sorts = 'created_at desc'
+    @users = @search.result(distinct: true)
+
+  end
+
   def show
     @user = User.find(params[:id])
   end
